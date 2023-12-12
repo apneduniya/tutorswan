e9b49437f62d5d0115d365c329951d4f31eff8a4
apneduniya <apneduniya.dontdisturb@gmail.com>
2023-12-12 16:39:09 +0530

    feat: Added favicon, title, download csv button in results page and also checking if auth then direct button for dashboard otherwise login and register page in navbar

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -6,10 +6,39 @@ import { Spin } from 'antd';
 
 function Results() {
 
-    const { id, name } = useParams();
     const navigate = useNavigate();
+    const { id, name } = useParams();
     const [results, setResults] = useState([]);
     const [loading, setLoading] = useState(false);
+
+
+    const convertToCSV = (data) => {
+        // Add header row
+        let csvContent = 'Branch,Roll No,Total Marks,Date & Time\n';
+    
+        // Add data rows
+        data.forEach((item) => {
+            const branch = item.student_branch || '';
+            const rollNo = item.student_rollno || '';
+            const totalMarks = item.total_marks? item.total_marks: item.total_marks === 0? 0: '';
+            const dateTime = item.created_at || '';
+    
+            csvContent += `${branch},${rollNo},${totalMarks},"${new Date(dateTime).toLocaleDateString() + " " + new Date(dateTime).toLocaleTimeString()}"\n`;
+        });
+    
+        return csvContent;
+    };
+    
+    const download = () => {
+        // Replace 'yourData' with the actual array of objects
+    
+        const csvContent = convertToCSV(results);
+        const encodedURI = encodeURI(`data:text/csv;charset=utf-8,${csvContent}`);
+        // window.open(encodedURI);
+
+        window.location.href = encodedURI;
+    };
+    
 
     useEffect(() => {
 
@@ -51,9 +80,14 @@ function Results() {
     return (
         <>
             <Spin spinning={loading}>
-                <h1 className='text-4xl font-bold text-center text-gray-800 mt-24'>
-                    Results
-                </h1>
+                <div className="flex items-center lg:justify-between flex-wrap gap-5 justify-center mt-24">
+                    <h1 className='text-4xl font-bold text-center text-gray-800'>
+                        Results
+                    </h1>
+                    <button onClick={download} className="w-[300px] h-fit bg-blue-500 hover:bg-blue-600 select-none text-white font-bold py-2 px-4 rounded">
+                        Download CSV
+                    </button>
+                </div>
                 <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-10">
                     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
